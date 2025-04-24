99616ae0001775cd38b0bc6b71e92c8617badbe2
NeiltonSeguins <seguins.neilton@gmail.com>
2025-04-24 16:35:05 -0300

    Cria o authprovider e as funções de signin, signup, sign out.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,12 +1,75 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [session, setSession] = useState(undefined);
+
+  // Sign up
+  const signUpNewUser = async (email, password) => {
+    const { data, error } = await supabase.auth.signUp({
+      email: email.toLowerCase(),
+      password: password,
+    });
+
+    if (error) {
+      console.error("Error signing up: ", error);
+      return { success: false, error };
+    }
+
+    return { success: true, data };
+  };
+
+  // Sign in
+  const signInUser = async (email, password) => {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: email.toLowerCase(),
+        password: password,
+      });
+
+      if (error) {
+        console.error("Sign-in error:", error.message);
+        return { success: false, error: error.message };
+      }
+
+      console.log("Sign-in success:", data);
+      return { success: true, data };
+    } catch (err) {
+      console.error("Unexpected error during sign-in:", err.message);
+      return {
+        success: false,
+        error: "An unexpected error occurred. Please try again.",
+      };
+    }
+  };
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  // Sign out
+  async function signOut() {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Error signing out:", error);
+    }
+  }
+
   return (
-    <AuthContext.Provider value={session}>{children}</AuthContext.Provider>
+    <AuthContext.Provider
+      value={{ signUpNewUser, signInUser, session, signOut }}
+    >
+      {children}
+    </AuthContext.Provider>
   );
 };
 
--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,8 @@
+import { createBrowserRouter } from "react-router";
 import App from "./App";
 import Signup from "./components/SignUp";
 import Signin from "./components/SignIn";
 import Home from "./routes/Home";
-import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
   { path: "/", element: <App /> },
